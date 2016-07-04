c9e17030822c34f97df93a5e6d6df28e89486515
Josephine <bluejello@gmail.com>
2016-07-04 00:18:33 -0700

    Implement functions _.every() through _.once()

--- src/underbar.js
+++ src/underbar.js
@@ -190,12 +190,34 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    if (iterator === undefined){
+      iterator = _.identity;
+    }
+    // var result = true;
+    // _.each(collection, function(element){
+    //   if (!iterator(element)){
+    //     result = false;
+    //   }
+    // });
+    // return result;
+    return _.reduce(collection, function(result, item){
+      if (!iterator(item)){
+        result = false;
+      }
+      return result;
+    },true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
+    if (iterator === undefined){
+      iterator = _.identity;
+    }
     // TIP: There's a very clever way to re-use every() here.
+    return !_.every(collection, function(item){
+      return !iterator(item);
+    });
   };
 
 
@@ -218,11 +240,27 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var args = Array.prototype.slice.call(arguments);
+    for (var i=1; i<args.length; i++){
+      for (var key in args[i]){
+        obj[key] = args[i][key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var args = Array.prototype.slice.call(arguments);
+    for (var i=1; i<args.length; i++){
+      for (var key in args[i]){
+        if (!(key in obj)) {
+          obj[key] = args[i][key];
+        }
+      }
+    }
+    return obj;
   };
 
 
